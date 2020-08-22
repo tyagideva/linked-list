be292bfb51e7acf510fc84be37f49b4d097f3da5
devashish <59768696+tyagideva@users.noreply.github.com>
2020-08-22 15:58:56 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,282 +1,127 @@
-#include<iostream>
+#include <iostream>
 #include<conio.h>
-#include<stdlib.h>
+#include<process.h>
 using namespace std;
-
-
-
-class List
+class poly
 {
-    struct node
-    {int data;
+
+     struct node
+
+    {int coff,exp;
      struct node* next;
-     struct node* pre;
-}
-
-     *temp,*first;
-public:
-    List()
-    {
-        first=NULL;
-       // last=NULL;
     }
-    void create();
-    void  delet_bet(int n);
-    void del_big();
-    void display();
-    void insert_end();
-    void Insert_bet(int data, int n);
-    void delet_last();
-    void insert_at_beg();
-    void Search(int n);
-    void Reverse();
+
+  *temp,*first,*last;
+
+  public:
+      poly()
+      {
+         first = NULL;
+         last = NULL;
+      }
+      void get_poly();
+      void add(poly pp , poly pp1);
+      void show();
+
 };
-void List::create()
-{  char ch ='y';
-    do{
-    temp=new node;
-    int n;
-    cout<<"\nEnter an Element:";
-    cin>>n;
-    temp->data=n;
-    temp->next=NULL;
-    temp->pre = NULL;
-    if(first==NULL)
-    {
-        first=temp;
-       // last=first;
-    }
+void poly :: get_poly()
+{
 
+    char ch = 'y';
+    do
+    {
+        int c,e;
+      temp = new node;
+        cout<<"\n enter the coffecient ";
+        cin>>c;
+        cout<<"\n enter the exp.";
+        cin>>e;
+        temp->coff = c;
+        temp->exp = e;
+        temp->next = NULL;
+        if(first == NULL)
+        {
+
+        first = temp;
+        last = first;
+        }
+        else
+        {  last->next = temp;
+            last = temp;
+
+        }
+        cout<<"\n if you want to continue press y";
+        cin>>ch;
+    }    while(ch=='y');
+
+}
+void poly:: show()
+{
+    temp = first;
+    while(temp!= NULL)
+    {
+        cout<<temp->coff<<"X^"<<temp->exp<<"+";
+        temp = temp->next;
+    }
+}
+void poly:: add(poly pp,poly pp1)
+
+{
+    node * p,*p1;
+    p = pp.first;
+    p1 = pp1.first;
+    cout<<p1->next;
+ while(p->next && p1->next)
+ { temp = new node;
+    if(p->exp > p1->exp)
+    {
+        temp->exp = p->exp;
+        temp->coff = p->coff;
+        p =  p->next;
+    }
+    else if(p1->exp > p->exp)
+    {
+        temp->exp = p1->exp;
+        temp->coff = p1->coff;
+        p1 =  p1->next;
+    }
+    else
+    {
+        temp->exp = p->exp;
+        temp->coff = p1->coff + p->coff;
+        p1 =  p1->next;
+        p1 =  p1->next;
+    }
+    if(first == NULL)
+       {
+
+        first = temp;
+        last = NULL;}
 
     else
     {
-
-       node *s;
-
-        s = first;
-        while(s->next != NULL)
-        {
-            s = s-> next;
-
-        }
-        s->next = temp;
-        temp-> pre = s;
-    }
-
-
-
-    cout<<"if you want to add more enter y else 0";
-    cin>>ch;
-}while(ch == 'y');
-}
-
-void List:: Insert_bet(int data, int n)
-{
-   temp = new node;
-   temp->data = data;
-   temp->next = NULL;
-   temp->pre = NULL;
-   if (n == 1){
-    temp->next = first;
-    first->pre = temp;
-    first = temp;
-    return;
-   }
-    node *temp2 ;
-   temp2 = first;
-   for (int i = 0; i < n-2; i++){// i feel like it doesn't even go through this loop
-     temp2 = temp2->next;
-   }
-   temp->next = temp2->next;
-   temp2->next->pre = temp;
-   temp->pre = temp2;
-   temp2->next = temp;
-
-}
-void List :: insert_end()
-{
-    temp = new node;
-    int n;
-    cout<<"\nEnter an Element:";
-    cin>>n;
-    temp->data=n;
-    temp->next=NULL;
-    temp->pre = NULL;
-
-    node *a;
-    a = first;
-    while(a->next != NULL)
-    {
-        a = a->next;
+        last->next = temp;
+        last = temp;
 
     }
-    a->next = temp;
-    temp->pre = a;
+ }
 
-}
+ }
 
-void List :: delet_bet(int n)
-{ node *q;
-    temp = new node;
-    temp = first;
-
-
-while (temp->next->next != NULL)
-    {
-
-        if (temp->next->data == n)
-        {
-            q = temp->next;
-            temp->next = q->next;
-            q->next->pre = temp;
-
-            free(q);
-            return;
-        }
-        temp = temp->next;
-    }
-}
-
-void List:: insert_at_beg()
-{    int n;
-    temp = new node;
-    cout<<"\n enter the element to be entered";
-    cin>>n;
-    temp->data = n;
-    temp->pre = NULL;
-    temp->next = first;
-    first->pre = temp;
-    first = temp;
-
-
-
-
-}
-void List:: delet_last()
-{    temp = first;
-   node *temp1;
-while(temp->next->next != NULL)
- temp = temp->next;
-
-temp1 = temp->next;
-temp->next = NULL;
-delete(temp1);
-
-
-
-
-
-}
-void List::display()
+ int main()
  {
-    temp = new node;
-     temp = first;
-    while(temp->next != NULL)
-    {
-        cout<<temp->data;
-        cout<<"-->";
-        temp = temp->next;
-    }
-    cout<<"-->"<<temp->data;
 
-
-    cout<<"NULL";
-}
-void List:: del_big()
-{   temp = new node;
-    temp = first;
-    first = first->next;
-    first->pre = NULL;
-    delete(temp);
+     poly p1,p2,p3;
+     p1.get_poly();
+     p1.show();
+     p2.get_poly();
+     p2.show();
+     p3.add(p1,p2);
+     p3.show();
+return 0;
+ }
 
 
 
-}
-void List ::Reverse()
-{
-    node *p1, *p2;
-    p1 = first;
-    p2 = p1->next;
-    p1->next = NULL;
-    p1->pre = p2;
-    while (p2 != NULL)
-    {
-        p2->pre = p2->next;
-        p2->next = p1;
-        p1 = p2;
-        p2 = p2->pre;
-    }
-    first = p1;
-}
 
 
-
-/*void List :: Search(int n)
-{ int a = 0;
-temp = new node;
-  temp = first;
-  while(temp->next != NULL)
-  {
-      a++;
-      if(temp->data == n)
-      {
-
-       cout<<"\n element present at"<<a;
-      //return;
-      //break;
-      }
-      temp = temp->next;
-
-  }
-  cout<<"\n element not found";
-
-
-}
-*/
-int main()
-{
-    List l;
-    int c;
-    while(1)
-    {
-
-        cout<<"\nEnter Your Choice:";
-        cin>>c;
-        switch(c)
-        {
-        case 1:
-            l.create();
-            break;
-        case 2:
-        l.insert_end();
-        case 3:
-            l.display();
-            break;
-        case 4:
-            l.insert_at_beg();
-            break;
-        case 5:
-            l.Insert_bet(0,3);
-            break;
-        case 6:
-            l.del_big();
-            break;
-        case 7:
-            l.delet_bet(3);
-            break;
-        case 8:
-            l.delet_last();
-            break;
-        case 9:
-           l.Search(5);
-           break;
-        case 10:
-            l.Reverse();
-            break;
-        default:
-        cout<<"enter right choice";
-
-        }
-    }
-    return 0;
-}
